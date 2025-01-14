015a674ce6d1d1d47b596dd7901a02799e5524fa
Toktamysov Timur <timur.toktamysov@gmail.com>
2025-01-14 14:42:53 +0300

    created buttons

--- services/handlers.py
+++ services/handlers.py
@@ -11,7 +11,20 @@ from aiogram.filters import Command
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 router = Router()
+default = ['Привет', 'Пока']
+'''kb = ReplyKeyboardBuilder()
+        kb.button(text="Таблица пользователей")
+        kb.button(text="Очистить таблицу")
+        kb.button(text="Ответить пользователю")
+        kb.adjust(1, 1, 1)
+        await msg.answer("Доступные Команды", reply_markup=kb.as_markup(resize_keyboard=True))'''
 
+def building_keyboard(custom=default):
+    kb = ReplyKeyboardBuilder()
+    for element in custom:
+        kb.button(text=element)
+    kb.adjust(1, len(custom))
+    return kb.as_markup(resize_keyboard=True)
 
 class update(StatesGroup):
     smth = State
@@ -19,9 +32,10 @@ class update(StatesGroup):
 @router.message(Command('start'))
 async def start_handler(msg: Message, state: FSMContext) -> None:
     if msg.from_user.id == ADMIN_ID:
-        await msg.answer("")
+        await msg.answer("Привет, администратор")
     else:
-        await msg.answer("")
+        await msg.answer("hello")
+        await msg.answer("choose", reply_markup=building_keyboard())
 
 
 @router.message(Command('menu'))
