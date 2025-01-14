b5a17a24e01845adaef4e4cbe1ebc1bfe786b650
Toktamysov Timur <timur.toktamysov@gmail.com>
2025-01-14 13:43:06 +0300

    adding handlers

--- services/bot.py
+++
@@ -1,5 +0,0 @@
-from .token import TOKEN
-from .cfg import *
-
-def sex():
-    print(TOKEN)
---
+++ services/handlers.py
@@ -0,0 +1,53 @@
+from .token import BOT_TOKEN, ADMIN_ID
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.types import *
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+router = Router()
+
+
+class update(StatesGroup):
+    smth = State
+
+@router.message(Command('start'))
+async def start_handler(msg: Message, state: FSMContext) -> None:
+    if msg.from_user.id == ADMIN_ID:
+        await msg.answer("")
+    else:
+        await msg.answer("")
+
+
+@router.message(Command('menu'))
+async def menu_handler(msg: Message, state: FSMContext) -> None:
+    await msg.answer('/activate')
+    await msg.answer('/terminate')
+    await msg.answer('/custom')
+    await msg.answer('/show')
+
+
+@router.message(Command('activate'))
+async def activate_handler(msg: Message, state: FSMContext) -> None:
+    await msg.answer('1')
+
+
+
+@router.message(Command('terminate'))
+async def terminate_handler(msg: Message, state: FSMContext) -> None:
+    await msg.answer('2')
+
+
+@router.message(Command('custom'))
+async def custom_handler(msg: Message, state: FSMContext) -> None:
+    await msg.answer('3')
+
+
+@router.message(Command('show'))
+async def show_handler(msg: Message, state: FSMContext) -> None:
+    await msg.answer('4')
